e135384588c24ad8b0e94336275a0d8f74e2f71b
Thuyensn2003 <thuyensn2003@gmail.com>
2023-08-28 20:32:44 +0700

    Lesson_01

---
+++ lesson_1/index.js
@@ -0,0 +1,34 @@
+//Biến (variable) và các kiểu dữ liệu cơ bản của Js :
+/*
+Có 8 kiểu dữ liệu có thể chứa trong 1 biến :
+
++ number : số , chữ số 
++ string : kí tự ,chuỗi kí tự 
++ boolean : kiểu logic true và false
++ biglnt : số nguyên 
++ undefined : chưa định nghĩa
++ Null : kiểu vô giá trị
++ symbol : kí hiệu 
++ Object : đối tượng
+*/
+
+let myNumber = 6;
+
+let myName = "Thuyen"; // string chứa trong ""
+
+// Yêu cầu hệ thống in ra dữ liệu của biến ( var ) bằng command :
+
+var myColor = "blue";
+
+const myAge = 2003; 
+
+// Khác biệt giữa let và const : khi đã xác định 1 var sử dụng hằng biến const sẽ không thể ghép kiểu dữ liệu khác cho biến đó
+
+// một số keyword sẽ không được sử dụng để đặt tên cho biến , ví dụ : docmument , let , ....
+
+console.log(myName);
+
+
+
+
+
